f838fcce0feaa0c8848dfbdca50da0a05747806f
Karl Mulcahy <karlmulcahy@outlook.com>
2022-11-27 23:09:37 +0000

    Wrote Unit Tests for Gym Contoller and Gym Repository

---
+++ GymApp.Tests/Controller/GymControllerTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GymApp.Controllers;
+using GymApp.Interfaces;
+using FakeItEasy;
+using Microsoft.AspNetCore.Http;
+using Xunit;
+using GymApp.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymApp.Tests.Controller
+{
+    //You want to return fake values so that the environment is the same
+    //Can't unit test static functions
+    public class GymControllerTests
+    {
+        private GymController _gymController;
+        private IGymRepository _gymRepository;
+        private IPhotoService _photoService;
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public GymControllerTests()
+        {
+            //Dependencies
+            _gymRepository = A.Fake<IGymRepository>();
+            _photoService = A.Fake<IPhotoService>();
+            _httpContextAccessor = A.Fake<HttpContextAccessor>();
+
+            //SUT
+            _gymController = new GymController(_gymRepository, _photoService, _httpContextAccessor);
+        }
+
+        [Fact]
+        public void GymController_Index_ReturnSuccess()
+        {
+            //Arrange - What do i need to bring in?
+            var gyms = A.Fake<IEnumerable<Gym>>();
+            A.CallTo(() => _gymRepository.GetAll()).Returns(gyms);
+            //Act
+            var result = _gymController.Index();
+            //Assert - Object check actions
+            result.Should().BeOfType<Task<IActionResult>>();
+        }
+        [Fact]
+        public void GymController_Detail_ReturnSuccess()
+        {
+            //Arrange
+            var id = 1;
+            var gym = A.Fake<Gym>();
+            A.CallTo(() => _gymRepository.GetByIdAsync(id)).Returns(gym);
+
+            //Act
+            var result = _gymController.Detail(id);
+            //Assert - Object check actions
+            result.Should().BeOfType<Task<IActionResult>>();
+
+        }
+    }
+}
+    
---
+++ GymApp.Tests/Repository/GymrRepositoryTests.cs
@@ -0,0 +1,134 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using GymApp.Data;
+using GymApp.Data.Enum;
+using GymApp.Models;
+using GymApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GymApp.Tests.Repository
+{
+    public class GymrRepositoryTests
+    {
+        private async Task<ApplicationDbContext> GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var databaseContext = new ApplicationDbContext(options);
+            databaseContext.Database.EnsureCreated();
+            if (await databaseContext.Gyms.CountAsync() < 0)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    databaseContext.Gyms.Add(
+                      new Gym()
+                      {
+                          Title = "Gym 1",
+                          Image = "https://www.shutterstock.com/shutterstock/photos/721723381/display_1500/stock-photo-modern-light-gym-sports-equipment-in-gym-barbells-of-different-weight-on-rack-721723381.jpg",
+                          Description = "This is the description of the first Gym",
+                          GymCategory = GymCategory.WeightLifting,
+                          Address = new Address()
+                          {
+                              Street = "123 Main St",
+                              City = "Dublin",
+                              Province = "Leinster"
+                          }
+                      });
+                    await databaseContext.SaveChangesAsync();
+                }
+            }
+            return databaseContext;
+        }
+        [Fact]
+        public async void GymRepository_Add_ReturnsBool()
+        {
+            //Arrange
+            var gym = new Gym()
+            {
+                Title = "Gym 1",
+                Image = "https://www.shutterstock.com/shutterstock/photos/721723381/display_1500/stock-photo-modern-light-gym-sports-equipment-in-gym-barbells-of-different-weight-on-rack-721723381.jpg",
+                Description = "This is the description of the first Gym",
+                GymCategory = GymCategory.WeightLifting,
+                Address = new Address()
+                {
+                    Street = "123 Main St",
+                    City = "Dublin",
+                    Province = "Leinster"
+                }
+            };
+            var dbContext = await GetDbContext();
+            var gymRepository = new GymRepository(dbContext);
+            //Act
+            var result = gymRepository.Add(gym);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+        [Fact]
+        public async void GymRepository_GetByIdAsync_ReturnsGym()
+        {
+            //Arrange
+            var id = 1;
+            var dbContext = await GetDbContext();
+            var gymRepository = new GymRepository(dbContext);
+
+            //Act
+            var result = gymRepository.GetByIdAsync(id);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<Task<Gym>>();
+        }
+        [Fact]
+        public async void GymRepository_GetAll_ReturnsList()
+        {
+            //Arrange
+            var dbContext = await GetDbContext();
+            var gymRepository = new GymRepository(dbContext);
+
+            //Act
+            var result = await gymRepository.GetAll();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<List<Gym>>();
+        }
+        [Fact]
+        public async void GymRepository_GetGymsByCity_ReturnsEmpty()
+        {
+            //Arrange
+            var city = "Limerick";
+            var gym = new Gym()
+            {
+                Title = "Gym 1",
+                Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
+                Description = "This is the description of the first gym",
+                GymCategory = GymCategory.PowerLifting,
+                Address = new Address()
+                {
+                    Street = "123 Main St",
+                    City = "Dublin",
+                    Province = "Leinster"
+                }
+            };
+            var dbContext = await GetDbContext();
+            var gymRepository = new GymRepository(dbContext);
+
+            //Act
+            gymRepository.Add(gym);
+            var result = await gymRepository.GetGymByCity(city);
+
+            //Assert - Should be empty as the city passed is not the same as the city on record
+            result.Should().BeEmpty();
+            //result.Should().BeOfType<List<Gym>>();
+            //result.First().Title.Should().Be("Gym 1");
+        }
+
+    }
+}
